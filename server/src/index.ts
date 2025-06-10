d278408b07ecb7c447523d7ab4394bb40e5f930a
milkyway <hoad3579@gmail.com>
2025-06-11 00:15:10 +0700

    commit 6

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 // CORS configuration
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production'
-    ? ['https://drawbattle.daongochoa.click']  // Chỉ cần domain HTTPS
+    ? ['https://drawbattle.daongochoa.click']
     : 'http://localhost:5173',
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
